bfead8960e34d4bf8043391d4ed450ac78ccc04c
Marcus Vinicius Miguel de Oliveira <marcusvinoli@gmail.com>
2023-08-25 04:05:30 -0300

    Added Endereço form

--- src/lib/Endereco.svelte
+++ src/lib/Endereco.svelte
@@ -1,13 +1,15 @@
-<script lang="js">
+<script lang="ts">
 // @ts-nocheck
 
-  import MaskInput from "svelte-input-mask/MaskInput.svelte";
+  import MaskedInput from "./utils/MaskedInput.svelte";
   import EstadoSelector from "./utils/EstadoSelector.svelte";
 
   import cep from "cep-promise";
 
+  let loading = '';
+  let errorMessage = ''
   let semNumero = false;
-  let cep2find = "";
+
   export let endereco = {
     cep: "",
     logradouro: "",
@@ -20,89 +22,103 @@
     estado: ""
   }
 
+  function startLoading() {
+    loading = 'loading';
+  }
+
+  function stopLoading() {
+    loading = '';
+  }
+
+  function loadResults(data) {
+    endereco.logradouro = data.street;
+    endereco.bairro = data.neighborhood;
+    endereco.cidade = data.city;
+    endereco.estado = data.state;
+    stopLoading();
+  }
+  
+  function loadError(e) {
+    console.log(e);
+    stopLoading();
+  }
+
   function loadCepInfo(inputCep = String) {
-    console.log(endereco);
-    console.log("Running CEP Promise for {}", inputCep);
-    cep(inputCep).then(
-      console.log
-    );
+    startLoading();
+    var cepData = endereco.cep.replace(/[^\d]/g, '');
+    console.log("Searching for" + cepData);
+    cep(cepData)
+    .then(
+      loadResults
+    )
+    .catch(
+      loadError
+    )
   }
-
-  function test() {
-    console.log(cep2find);
-  }
-
-  function debounce(func, timeout = 300){
-    let timer;
-    return (...args) => {
-      clearTimeout(timer);
-      timer = setTimeout(() => { func.apply(this, args); }, timeout);
-    };
-  }
-
-  function saveInput(){
-    console.log('Saving data');
-  }
-  const processChange = () => debounce(() => saveInput());
-
 </script>
 
 <div class="w-full">
   <div class="flex w-full">
     <div class="p-1">
       <label for="cep" class="label">CEP</label>
-      <MaskInput id="cep" type="text" class="input input-bordered w-50" placeholder="CEP" maskChar="_" mask="00.000-000" bind:value={cep2find} onkeyup="processChange()"/>
-      <button class="btn" on:click={test}>Carregar informações do CEP</button>
+      <div class="flex flex-row w-full">
+        <MaskedInput id="cep" type="text" bind:value={endereco.cep} customClass="input input-bordered w-50 p-1" mask="##.###-###"/>
+        <button class="btn ml-2" on:click={loadCepInfo}>
+          <span class="{loading} loading-spinner text-primary"></span>
+          Pesquisar
+          <span class=""></span>
+        </button>
+      </div>
     </div>
-  </div>
-  <div class="flex w-full">
     <div class="grow p-1">
       <label for="cep" class="label">Logradouro</label>
-      <input id="cep" type="text" placeholder="Logradouro" class="input input-bordered w-full" />
+      <input id="cep" type="text" placeholder="Logradouro" class="input input-bordered w-full" bind:value={endereco.logradouro}/>
     </div>
     <div class="flex flex-row">
-      <div class="align-center p-1">
+      <div class="content-center p-1">
         <label for="semNumero" class="label">Sem Número</label>
-        <input id="semNumero" type="checkbox" bind:checked="{semNumero}" class="checkbox" />
+        <div class="p-2" style="text-align: center;">
+          <input id="semNumero" type="checkbox" bind:checked="{semNumero}" class="checkbox checkbox-lg" />
+        </div>
       </div>
       <div class="p-1">
         <label for="numero" class="label">Número</label>
         {#if semNumero}
           <input id="numero" type="text" placeholder="Nº" class="input input-bordered w-24" value="S/N" disabled/>
         {:else}
-          <input id="numero" type="text" placeholder="Nº" class="input input-bordered w-24" />
+          <input id="numero" type="text" placeholder="Nº" class="input input-bordered w-24" bind:value={endereco.numero}/>
         {/if}
       </div>
     </div>
   </div>
   <div class="flex w-full">
     <div class="p-1 w-28">
-      <label for="cep" class="label">Qd.</label>
-      <input id="cep" type="text" placeholder="Qd." class="input input-bordered w-full" />
+      <label for="qd" class="label">Qd.</label>
+      <input id="qd" type="text" placeholder="Qd." class="input input-bordered w-full" />
     </div>
     <div class="p-1 w-28">
-      <label for="cep" class="label">Lt.</label>
-      <input id="cep" type="text" placeholder="Lt." class="input input-bordered w-full" />
+      <label for="lt" class="label">Lt.</label>
+      <input id="lt" type="text" placeholder="Lt." class="input input-bordered w-full" />
     </div>
     <div class="p-1 grow">
-      <label for="cep" class="label">Complemento</label>
-      <input id="cep" type="text" placeholder="Complemento" class="input input-bordered w-full" />
+      <label for="complemento" class="label">Complemento</label>
+      <input id="complemento" type="text" placeholder="Complemento" class="input input-bordered w-full" />
     </div>
   </div>
   <div class="flex w-full">
   </div>
   <div class="flex w-full">
-    <div class="p-1">
-      <label for="cep" class="label">Bairro</label>
-      <input id="bairro" type="text" placeholder="Bairro" class="input input-bordered w-full" />
+    <div class="p-1 grow">
+      <label for="bairro" class="label">Bairro</label>
+      <input id="bairro" type="text" placeholder="Bairro" class="input input-bordered w-full" bind:value={endereco.bairro}/>
+    </div>
+    <div class="p-1 grow">
+      <label for="cidade" class="label">Cidade</label>
+      <input id="cidade" type="text" placeholder="Cidade" class="input input-bordered w-full" bind:value={endereco.cidade}/>
     </div>
     <div class="p-1">
-      <label for="cep" class="label">Cidade</label>
-      <input id="cidade" type="text" placeholder="Cidade" class="input input-bordered w-full" />
-    </div>
-    <div class="p-1">
-      <label for="cep" class="label">Estado</label>
-      <EstadoSelector id="estado"/>
+      <label for="estado" class="label">Estado</label>
+      <EstadoSelector id="estado" bind:value={endereco.estado}/>
     </div>
   </div>
 </div>
